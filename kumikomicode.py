a9f1c27fa2de1b31502633512f25a6dd75c49a8a
Daitetsu <daitetsu@telhi.co.jp>
2020-04-28 16:04:41 +0900

    Initial commit

---
+++ kumikomicode.py
@@ -0,0 +1,24 @@
+import chromedriver_binary
+import time
+from selenium import webdriver
+import re
+
+driver = webdriver.Chrome()
+
+while True:
+    driver.get('https://wall.sli.do/event/qzahwoxu?section=e161f411-c5b3-4b06-a3f0-94c82133ee51')
+    time.sleep(5)
+    texts = driver.find_elements_by_tag_name("fit-text")
+    num = None
+    for t in texts:
+        num = re.match(r'[0-9]{9}', t.text)
+        if num: # 数字9桁で構成される文字列が見つかった場合
+            print(num)
+            # ここに処理を入れれば自動出席もできる
+            break
+
+    time.sleep(60)
+    if num:
+        break
+
+driver.quit()
